594822ba192fb76f266512f1b4913299008791bb
dyg <dyg@192.168.139.1>
2019-12-18 21:30:43 +0800

    8皇后问题

--- DataStructure/src/com/dyg/datestucture/recursion/Recursion.java
+++ DataStructure/src/com/dyg/datestucture/recursion/Recursion.java
@@ -1,5 +1,6 @@
 package com.dyg.datestucture.recursion;
-/*java  实现递归
+
+/*java  实现递归  需要多实践，解决大量循环的问题
 @author   dyg
 递归1、执行一个方法，建立一个保护的栈空间
 2、方法的内部的基本数据类型不共享
@@ -13,21 +14,25 @@ public class Recursion {
 		/*
 		 * System.out.println(method2(4)); method1(4);
 		 */
-	//	list(createmap());
-		System.out.println("");
-		int  map[][]=createmap();
+		// list(createmap());
+		/*System.out.println("");
+		int map[][] = createmap();
 		move(map, 1, 1);
-		list(map);
+		list(map);*/
+	
+		queen(0);
+		System.out.println(count);
 	}
-     
-	//递归案例1
+
+	// 递归案例1
 	public static void method1(int n) {
 		if (n > 2) {
 			method1(n - 1);
 		}
 		System.out.println(n);
 	}
-	//递归案例2
+
+	// 递归案例2
 	public static int method2(int n) {
 		if (n == 1) {
 			return n;
@@ -44,20 +49,21 @@ public class Recursion {
 			System.out.println();
 		}
 	}
-	//递归案例3 走迷宫
+
+	// 递归案例3 走迷宫
 	public static boolean move(int arr[][], int col, int row) {
 		if (arr[8][6] == 2) {
 			return true;
 		} else {
 			if (arr[col][row] == 0) {
 				arr[col][row] = 2;
-				if (move(arr, col +1, row)) {
+				if (move(arr, col + 1, row)) {
 					return true;
-				} else if (move(arr, col , row+1)) {
+				} else if (move(arr, col, row + 1)) {
 					return true;
-				} else if (move(arr, col-1, row - 1)) {
+				} else if (move(arr, col - 1, row - 1)) {
 					return true;
-				} else if (move(arr, col, row -1)) {
+				} else if (move(arr, col, row - 1)) {
 					return true;
 				} else {
 					arr[col][row] = 3;
@@ -70,6 +76,7 @@ public class Recursion {
 
 	}
 
+	// 初始化迷宫地图
 	public static int[][] createmap() {
 		int arr[][] = new int[10][8];
 
@@ -85,4 +92,42 @@ public class Recursion {
 		arr[3][2] = 1;
 		return arr;
 	}
+
+	static int count = 0;// 8皇后的结果总和
+	static int maxsize=8;
+	static  int arr[]= new  int[maxsize];// 存放8皇后的结果，下标表示行，arr[i]表示列
+	// 案例4-- 8皇后问题
+	public static  void queen(int n) {
+		if(n==8){
+			listqueen();
+			return;
+		}
+		for (int i = 0; i < maxsize; i++) {
+			arr[n] = i;
+			if (ismove(n)) {
+				queen(n + 1);
+			}
+		}
+
+	}
+
+	// 判断当前的
+	public static boolean ismove(int n) {
+		for (int i = 0; i < n; i++) {
+			if (arr[n] == arr[i] || Math.abs(n - i) == Math.abs(arr[n] - arr[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void listqueen() {
+		count++;
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+		}
+		System.out.println();
+
+	}
+
 }
